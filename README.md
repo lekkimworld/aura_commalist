1f5bf53e39b9232c8fab6567ffdfbc80c534455d
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2017-07-12 23:25:25 +0200

    Added example w/ accounts

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # aura_commalist
 Salesforce Lightning Component to create comma separated lists
 
+To deploy to a Salesforce org use the below button and then grant access to the deployed tab (CommaListExample).
+
 <a href="https://githubsfdeploy.herokuapp.com/app/githubdeploy/lekkimworld/aura_commalist">
   <img alt="Deploy to Salesforce"
        src="https://raw.githubusercontent.com/afawcett/githubsfdeploy/master/src/main/webapp/resources/img/deploy.png">
--- src/aura/CommaListExample/CommaListExampleController.js
+++ src/aura/CommaListExample/CommaListExampleController.js
@@ -1,9 +1,23 @@
 ({
 	doinit: function(component, event, helper) {
+		// array of strings
 		component.set("v.strings", ['foo', 'bar', 'baz']);
+
+		// array of objects
         component.set("v.objects", [
             {Name: 'foo', Link: 'https://www.salesforce.com?1', Child: {Link: 'https://developer.salesforce.com'}}, 
             {Name: 'bar', Link: 'https://www.salesforce.com?2', Child: {Link: 'https://developer.salesforce.com'}}, 
             {Name: 'baz', Link: 'https://www.salesforce.com?3', Child: {Link: 'https://developer.salesforce.com'}}]);
+
+        // get accounts
+        const action = component.get('c.getAccounts');
+        action.setCallback(this, function(response) {
+        	if (response.getState() === 'SUCCESS') {
+        		component.set('v.accounts', response.getReturnValue());
+        	} else {
+        		console.log('Bummer...');
+        	}
+        });
+        $A.enqueueAction(action);
 	}
 })
